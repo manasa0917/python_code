b5b9b9ccabeb7c9f9386c3f6d2b913bf1c7b00c0
manasa0917 <manasa.shivananchi@gmail.com>
2018-12-01 15:24:45 +0530

    Class 1 code

---
+++ Class1/Takeinput.py
@@ -0,0 +1,5 @@
+num1=input("Please enter number 1:")
+num2=input("Please enter number 2:")
+sum=int(num1)+int(num2)
+
+print(sum)
---
+++ Class1/addition.py
@@ -0,0 +1,11 @@
+a = 12
+b = 3
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a*b)
+print(a%b)
+
+for i in range (0,int(a/b)):
+    print (i)
