0cc84254c31a3197b37ee7b19bfcfd887a1a2118
manasa0917 <manasa.shivananchi@gmail.com>
2018-12-02 14:29:09 +0530

    commit

--- Class1/palindrome.py
+++ Class1/palindrome.py
@@ -1,6 +1,7 @@
 mystring = input("please input a string?:")
 reverse = mystring[-1::-1]
 if reverse == mystring:
-    print("Input String is a Palindrome")
+   print("Input String is a Palindrome")
 else:
-    print("Input String is not a Palindrome")
+   print("Input String is not a Palindrome")
+print("it is a palindrome")
