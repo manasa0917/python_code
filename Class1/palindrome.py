01658c50e7c06ac3a270e76f41dcb3ce42bb2da4
manasa0917 <manasa.shivananchi@gmail.com>
2018-12-02 14:58:21 +0530

    commit

--- Class1/palindrome.py
+++ Class1/palindrome.py
@@ -5,3 +5,15 @@ if reverse == mystring:
 else:
    print("Input String is not a Palindrome")
 print("it is a palindrome")
+
+
+data=input("Enter the input:")
+reverse2=data[len(data)-1::-1]
+reverse2=data[-1:-1]
+if reverse2==data:
+    print("input string {} is a palindrome".format(data))
+
+
+else:
+    print("input string is not a palindrome")
+print("goodbye")
