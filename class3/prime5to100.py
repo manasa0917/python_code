5df78f38ce1b841cc1b91fca0ec5abef609a9011
manasa0917 <manasa.shivananchi@gmail.com>
2018-12-08 16:38:06 +0530

    last commit

--- class3/prime5to100.py
+++ class3/prime5to100.py
@@ -1,6 +1,8 @@
+#file=open("prime_numbers.txt","a")
 start=int(input("Enter starting number(>5:"))
 end=int(input("Enter a ending number(<100000:"))
 primecount=0
+output_string=""
 for i in range(start,end+1):
     prime=1
     for j in range(2,i-1):
@@ -10,7 +12,15 @@ for i in range(start,end+1):
 
     if prime == 1:
         primecount=primecount+1
-        print("{} is prime".format(i))
+        if (primecount%10== 0):
+            output_string=output_string+"\n"
+        output_string=output_string+" "+str(i)
+        #print("{} is prime".format(i))
     else:
-        print("{} is  not prime".format(i))
+        #print("{} is  not prime".format(i))
+        pass
 print("{} is the count of prime numbers".format(primecount))
+print("Total {} prime numbers found.They are given below".format(primecount))
+print(output_string)
+file=open("prime_number_{}_to_{}.txt".format(start,end),"w")
+file.write(output_string)
