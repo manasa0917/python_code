74c34b284dc51e1f762c952981fee35a249e0719
manasa0917 <manasa.shivananchi@gmail.com>
2019-01-19 16:28:56 +0530

    commit

--- class 7/create_login_system.py
+++ class 7/create_login_system.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+import base64
 def getData(phone_no):
     mydb = mysql.connector.connect(
         host="localhost",
@@ -34,21 +34,28 @@ def createNewUser():
     name = input("Name : ")
     addr = input("Address: ")
     pwd = input("Password: ")
+    encoded = base64.b64encode(pwd.encode())
+    print(encoded)
     data = input("Tell my about yourself : ")
     sql = "INSERT INTO customers (phone_no,name, address,pwd,data) VALUES (%s, %s,%s,%s,%s)"
-    val = (phone_no,name,addr,pwd,data)
+    val = (phone_no,name,addr,encoded,data)
     insert(sql,val)
 
 def loginAndShowData():
     phone_no = input("Phone No: ")
     pwd = input("Password : ")
     value1 = getData(phone_no)
+    if not  value1:
+        print("User name or password doesnt exist.Try again")
+        return
     value = value1[0]
     print(value)
     stored_pwd = value[3]
-    if(stored_pwd == pwd):
+    stored_pwd_encoded = base64.b64encode(pwd.encode())
+    if(stored_pwd == stored_pwd_encoded):
         print("Login success")
         print(value[4])
+
 
 
 choice = int(input("Sign up (1) or Login (2)"))
